01caa31ac7a9f0207b468e595ad9b635a80e6e56
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-08-03 17:05:18 -0300

    adjust component

---
+++ src/components/input/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ErrorMessage = styled.p`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  font-size: 0.75rem;
+  p {
+    text-align: center;
+    color: #af1919;
+  }
+`;
