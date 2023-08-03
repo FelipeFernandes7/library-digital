17a0bfad625edea89d4db6374de97ca121f518d0
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-08-03 17:04:42 -0300

    add auth provider context

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { RouterProvider } from 'react-router-dom'
-import { GlobalStyles } from './styles'
-import { router } from './App'
-import { Toaster } from 'react-hot-toast'
+import { RouterProvider } from "react-router-dom";
+import { GlobalStyles } from "./styles";
+import { router } from "./App";
+import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthContext";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalStyles />
     <Toaster position="top-center" reverseOrder={false} />
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
